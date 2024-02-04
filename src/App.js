9a4563019b160a97ff2c78b076c85143b593bda7
Nolan Zhong <nolan.zhong@duke.edu>
2024-02-03 23:33:37 -0500

    initial layout and codebase prepared

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import { CssBaseline, Grid } from '@material-ui/core';
+
+import Header from './components/Header/Header';
+import List from './components/List/List';
+import Map from './components/Map/Map';
 
 const App = () => {
     return (
-        <div>
-            <h1>Hello, World!</h1>
-        </div>
+        <>
+            <div>
+                <CssBaseline />
+                <Header />
+                <Grid container spacing={3} style={{ width: '100%' }}>
+                    <Grid item xc={12} md={4}>
+                        {/* Contains list components on left side */}
+                        <List />
+                    </Grid>
+
+                    <Grid item xc={12} md={8}>
+                        {/* Contains map */}
+                        <Map />
+                    </Grid>
+                </Grid>
+            </div>
+        </>
     )
 }
 
